6c548ff083820f0d46a635b2a1184024de61fc30
Drew Taylor <drew@infiniteinternet.ca>
2019-02-16 03:04:27 -0500

    router added to app

--- app/buidlingblocks/src/app/app.module.ts
+++ app/buidlingblocks/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
