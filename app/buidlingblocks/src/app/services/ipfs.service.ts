c302b1db61b06d20527c1c71a20a90cd2fa8a97e
Drew Taylor <drew@infiniteinternet.ca>
2019-02-16 12:46:19 -0500

    add missing file

---
+++ app/buidlingblocks/src/app/services/ipfs.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+declare let require: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IpfsService {
+
+  public IPFS = require('ipfs-api');
+  public ipfs: any;
+
+  constructor() {
+  }
+
+  public bootstrap(): void {
+    this.ipfs = new this.IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+    console.log('this.ipfs =>', this.ipfs);
+  }
+}
