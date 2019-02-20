1c9aacf84f0b73c4dbd49ff21c9ade1b699c366b
Drew Taylor <drew@infiniteinternet.ca>
2019-02-19 19:58:57 -0500

    render images of courses more or less, pdfs not downloading correctly because of responseType not being set

--- app/buidlingblocks/src/app/app.module.ts
+++ app/buidlingblocks/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { EntryComponent } from './entry/entry.component';
@@ -34,7 +35,8 @@ import { TeacherViewCourseDataComponent } from './teacher-view-course-data/teach
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/buidlingblocks/src/app/student-taking-course/student-taking-course.component.ts
+++ app/buidlingblocks/src/app/student-taking-course/student-taking-course.component.ts
@@ -3,8 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { ContractService } from '../services/contract.service';
 import { IpfsService } from '../services/ipfs.service';
-import { Http, ResponseContentType } from '@angular/http';
+//import { Http, ResponseContentType, RequestOptions } from '@angular/http';
+import { HttpClient } from "@angular/common/http";
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { map } from "rxjs/operators";
 
 declare let window: any;
 
@@ -38,7 +40,8 @@ export class StudentTakingCourseComponent implements OnInit {
     private authService: AuthService,
     private contractService: ContractService,
     private ipfsService: IpfsService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private http: HttpClient
   ) {
     this.authService.login(false);
     this.routerContext = this.route.snapshot.url;
@@ -46,6 +49,8 @@ export class StudentTakingCourseComponent implements OnInit {
   }
 
   async ngOnInit() {
+    let fileHash = null;
+    let context = null;
     this.courseData = await this.contractService.getCourseData(this.courseIndex);
     console.log('courseData',this.courseData);
 
@@ -53,20 +58,72 @@ export class StudentTakingCourseComponent implements OnInit {
 
     stream.on('data', (file) => {
       // write the file's path and contents to standard out
-      console.log(file.path)
       if(file.type !== 'dir') {
         file.content.on('data', (data) => {
           this.courseMaterial = JSON.parse(data.toString());
           console.log('courseMaterial', this.courseMaterial);
-          this.createImage(this.courseMaterial.steps[0].file)
+          // Generate steps images
+          for (var i = 0; i < this.courseMaterial.steps.length; i++) {
+            fileHash = this.courseMaterial.steps[i].file;
+            context = 'steps';
+            this.createImage(fileHash, context, i);
+          }
+          // Generate exam images
+          for (var j = 0; j < this.courseMaterial.exams.length; j++) {
+            fileHash = this.courseMaterial.exams[j].file;
+            context = 'exams';
+            this.createImage(fileHash, context, j);
+          }
         })
         file.content.resume()
       }
     });
   }
 
-  createImage(ipfsHash: string) {
-    // ...
+  createImage(ipfsHash: string, context: string, index: number) {
+    let blob = null;
+    let img = null;
+    let url = null;
+    let stream = this.ipfsService.readFileAsStream(ipfsHash);
+    stream.on('data', (file) => {
+      if(file.type !== 'dir') {
+        file.content.on('data', (data) => {
+          switch (context) {
+            case 'steps':
+              // Declare blob
+              blob = new window.Blob([data]);
+              // Declare URL for data blob
+              url = window.URL.createObjectURL(blob);
+              // Create image / document blob
+              img = new Image();
+              img.src = url;
+              window.setTimeout(() => {
+                this.courseMaterial.steps[index].img = this.getSantizeUrl(img.src);
+              }, 0);
+              break;
+            case 'exams':
+              // Declare blob
+              blob = new window.Blob([data], {type: "octet/stream"});
+              // Declare URL for data blob
+              url = window.URL.createObjectURL(blob);
+              window.setTimeout(() => {
+                this.courseMaterial.exams[index].file = this.getSantizeUrl(url);
+              }, 0);
+              break;
+          }
+        })
+        file.content.resume()
+      }
+    });
+  }
+
+  public downloadPDF (filePath) {
+    console.log('download?', filePath);
+    return this.http.get(filePath, { responseType: 'blob' });
+  }
+
+  private getSantizeUrl(url : string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 }
