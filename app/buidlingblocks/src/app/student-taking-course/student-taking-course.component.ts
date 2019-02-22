2b71773d2104a4f09afd060e64066e62be0ee5bb
Drew Taylor <drew@infiniteinternet.ca>
2019-02-21 22:17:36 -0500

    unverified test scores (pre-test results) now working

--- app/buidlingblocks/src/app/student-taking-course/student-taking-course.component.ts
+++ app/buidlingblocks/src/app/student-taking-course/student-taking-course.component.ts
@@ -26,8 +26,12 @@ export class StudentTakingCourseComponent implements OnInit {
     steps: null,
     exams: null,
     answers: null,
-    studentAnswers: null
+    studentAnswers: null,
+    unverifiedTestScores: null,
+    unsubmittedTestGrade: null
   };
+
+  public resultsReadyToSubmit: boolean = false;
 
   public courseIndex;
   public courseAddress;
@@ -61,7 +65,7 @@ export class StudentTakingCourseComponent implements OnInit {
     let stream = this.ipfsService.readFileAsStream(this.courseData.ipfsHash);
     stream.on('data', (file) => {
       // write the file's path and contents to standard out
-      if(file.type !== 'dir') {
+      if (file.type !== 'dir') {
         file.content.on('data', (data: any) => {
           this.courseMaterial = JSON.parse(data.toString());
           console.log('courseMaterial', this.courseMaterial);
@@ -115,6 +119,27 @@ export class StudentTakingCourseComponent implements OnInit {
     }, 0);
   }
 
+  public checkTestResults(): void {
+    console.log('courseMaterial', this.courseMaterial);
+    // Get individual scores
+    let unsubmittedTestScore = [];
+    let correctAnswers = 0;
+    // Compare Student answers with Teacher submitted answers
+    for (var i = 0; i < this.courseMaterial.answers.length; i++) {
+      if (this.courseMaterial.studentAnswers[i] == this.courseMaterial.answers[i].value) {
+        unsubmittedTestScore[i] = 1;
+        ++correctAnswers;
+      } else {
+        unsubmittedTestScore[i] = 0;
+      }
+    }
+    this.courseMaterial.unverifiedTestScores = unsubmittedTestScore;
+    // Get overall grade and metadata
+    this.courseMaterial.unsubmittedTestGrade = this.toUnsubmittedGrade(unsubmittedTestScore.length, correctAnswers);;
+    // Reset test form
+    //this.courseMaterial.studentAnswers = new Array(this.courseMaterial.answers.length);
+  }
+
   private getSafeFilename(filename: string) {
     let safeFilename = filename.replace(/[^\w\s]/gi, '');
     return safeFilename;
@@ -122,6 +147,72 @@ export class StudentTakingCourseComponent implements OnInit {
 
   private getSantizeUrl(url : string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  /**
+   * 100% A+ Pefect Score!
+   * 90-100% A+	Superb!
+   * 80-89% A	Excellent
+   * 70-79%	B	Very good
+   * 60-69%	C	Good
+   * 50-59%	D	Average
+   * 40-49%	F	Needs Work
+   * 0-39%	F Fail
+   * @param totalQuestions {Number}: Total number of questions being scored
+   * @param totalCorrectAnswers {Number}: Total correct answers submitted
+   */
+  private toUnsubmittedGrade(totalQuestions: number, totalCorrectAnswers: number) {
+    // Grade as percentage
+    let result = (totalCorrectAnswers / totalQuestions) * 100;
+    // Pass / Fail
+    let passingGrade = (result > 49) ? true : false;
+    // Assemble a letter grade and remark
+    let grade = null;
+    let remark = null;
+    if (result === 100) {
+      grade = "A++";
+      remark = "Perfect Score!";
+    } else if (this.numberInRange(result, 90, 99)) {
+      grade = "A+";
+      remark = "Superb!";
+    } else if (this.numberInRange(result, 80, 89)) {
+      grade = "A";
+      remark = "Excellent!";
+    } else if (this.numberInRange(result, 70, 79)) {
+      grade = "B";
+      remark = "Very Good";
+    } else if (this.numberInRange(result, 60, 69)) {
+      grade = "C";
+      remark = "Good";
+    } else if (this.numberInRange(result, 50, 59)) {
+      grade = "D";
+      remark = "Average";
+    } else if (result < 50) {
+      grade = "F";
+      remark = "Fail";
+    }
+    // Build output
+    let unsubmittedGrade = {
+      result: result.toFixed(1),
+      totalCorrectAnswers: totalCorrectAnswers,
+      totalQuestions: totalQuestions,
+      passingGrade: passingGrade,
+      grade: grade,
+      remark: remark
+    };
+    // Return unsubmittedGrade object
+    return unsubmittedGrade;
+  }
+
+  /**
+   * Helper function, determines whether a given number X is between a 
+   * given minimum and maximum threshold
+   * @param x {Number}: The number that is or isn't between min and max
+   * @param min {Number}: The number that x is or isn't below
+   * @param max {Number}: The number that x is or isn't above
+   */
+  private numberInRange(x: number, min: number, max: number): boolean {
+    return x >= min && x <= max;
   }
 
 }
