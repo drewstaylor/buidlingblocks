e288b5acec3fb416106bec73b18b4161b07c5907
Drew Taylor <drew@infiniteinternet.ca>
2019-02-21 19:48:56 -0500

    add view my lessons for teachers. TODO: add compare student results section

--- app/buidlingblocks/src/app/services/child-contract.service.ts
+++ app/buidlingblocks/src/app/services/child-contract.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HasherService } from './hasher.service';
+import { Buffer } from 'buffer';
 import bs58 from 'bs58';
 
 declare let Web3: any;
@@ -52,10 +53,14 @@ export class ChildContractService {
     this.ContractAddress = contractAddress;
     this.bootstrap();
     this.setContractAddress(contractAddress);
-    return this.ContractInstance
-              .methods
-              .getCourseData()
-              .call({from: window.userAccount});
+    let courseData = await this.ContractInstance
+                            .methods
+                            .getCourseData()
+                            .call({from: window.userAccount});
+    // Unpack valid IPFS CID 
+    let ipfsHash = this.getIpfsHashFromBytes32(courseData[0]);
+    courseData[0] = ipfsHash;
+    return courseData;
   }
 
 
@@ -133,5 +138,18 @@ export class ChildContractService {
   // "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
   private getBytes32FromIpfsHash(ipfsListing) {
     return "0x" + bs58.decode(ipfsListing).slice(2).toString('hex');
+  }
+
+  // Return base58 encoded ipfs hash from bytes32 hex string,
+  // E.g. "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
+  // --> "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL"
+  private getIpfsHashFromBytes32(bytes32Hex) {
+    // Add our default ipfs values for first 2 bytes:
+    // function:0x12=sha2, size:0x20=256 bits
+    // and cut off leading "0x"
+    const hashHex = "1220" + bytes32Hex.slice(2);
+    const hashBytes = Buffer.from(hashHex, 'hex');
+    const hashStr = bs58.encode(hashBytes);
+    return hashStr;
   }
 }
--- app/buidlingblocks/src/app/teacher-view-course-data/teacher-view-course-data.component.ts
+++ app/buidlingblocks/src/app/teacher-view-course-data/teacher-view-course-data.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { ContractService } from '../services/contract.service';
+import { IpfsService } from '../services/ipfs.service';
 import { ChildContractService } from '../services/child-contract.service';
 
 declare let window: any;
@@ -38,7 +39,9 @@ export class TeacherViewCourseDataComponent implements OnInit {
     private authService: AuthService,
     private route: ActivatedRoute,
     private courseContractService: ChildContractService,
-    private changeDetector: ChangeDetectorRef
+    private ipfsService: IpfsService,
+    private changeDetector: ChangeDetectorRef,
+    private sanitizer: DomSanitizer
   ) { 
     this.authService.login(false);
     this.routerContext = this.route.snapshot.url;
@@ -46,13 +49,11 @@ export class TeacherViewCourseDataComponent implements OnInit {
   }
 
   async ngOnInit() {
-    console.log('CA =>', this.courseAddress);
     this.loadCourse(this.courseAddress);
   }
 
   public async loadCourse (courseAddress: string) {
     // Reset courseData
-    
     let courseData = await this.courseContractService.getCourseData(courseAddress);
     console.log('courseData', [courseAddress, courseData]);
     this.courseData.name = courseData[1];
@@ -61,5 +62,74 @@ export class TeacherViewCourseDataComponent implements OnInit {
     this.courseData.ipfsHash = courseData[0];
     this.courseData.courseAddress = this.courseAddress;
     
+
+    // Load IPFS data
+    let fileHash = null;
+    console.log([typeof courseData[0], courseData[0]])
+    let stream = this.ipfsService.readFileAsStream(courseData[0]);
+    stream.on('data', (file) => {
+      // Write the file's path and contents to standard out
+      if (file.type !== 'dir') {
+        file.content.on('data', (data: any) => {
+          this.courseMaterial = JSON.parse(data.toString());
+          console.log('courseMaterial', this.courseMaterial);
+          // Generate steps images
+          for (var i = 0; i < this.courseMaterial.steps.length; i++) {
+            fileHash = this.courseMaterial.steps[i].file;
+            this.createImage(fileHash, i);
+          }
+          // Generate exam images
+          for (var j = 0; j < this.courseMaterial.exams.length; j++) {
+            fileHash = this.courseMaterial.exams[j].file;
+            this.createPDF(fileHash, j);
+          }
+          // Generate student answer models
+          this.courseMaterial.studentAnswers = new Array(this.courseMaterial.answers.length);
+        })
+        file.content.resume()
+      }
+    });
+
+  }
+
+  async createImage(ipfsHash: string, index: number) {
+    let blob = null;
+    let img = null;
+    let url = null;
+    const data = await this.ipfsService.cat(ipfsHash);
+    // Declare blob
+    blob = new window.Blob([data]);
+    // Declare URL for data blob
+    url = window.URL.createObjectURL(blob);
+    // Create image / document blob
+    img = new Image();
+    img.src = url;
+    window.setTimeout(() => {
+      this.courseMaterial.steps[index].img = this.getSantizeUrl(img.src);
+      this.changeDetector.detectChanges();
+    }, 0);
+  }
+
+  async createPDF(ipfsHash: string, index: number) {
+    let blob = null;
+    let url = null;
+    const pdf = await this.ipfsService.cat(ipfsHash);
+    // Declare blob
+    blob = new window.Blob([pdf], {type: "application/pdf"});
+    // Declare URL for data blob
+    url = window.URL.createObjectURL(blob);
+    window.setTimeout(() => {
+      this.courseMaterial.exams[index].file = this.getSantizeUrl(url);
+      this.changeDetector.detectChanges();
+    }, 0);
+  }
+
+  private getSafeFilename(filename: string) {
+    let safeFilename = filename.replace(/[^\w\s]/gi, '');
+    return safeFilename;
+  }
+
+  private getSantizeUrl(url : string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
